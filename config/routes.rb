069eb0ad6c993b43b9e32da1a564a12037699d00
Luiz Gustavo L Moura <lglmoura@gmail.com>
2016-04-25 17:37:21 -0300

    Corrigindo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :produtos
   resources :fabricantes
-  resources :fabricantes
+  
   root to: 'visitors#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
